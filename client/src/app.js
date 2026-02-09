70c93d41b759d461a6a1117a0605d61d7cfdac05
JW5123 <Jay2002714@gmail.com>
2026-02-09 23:43:44 +0800

    upload front-end app.js file

---
+++ client/src/app.js
@@ -0,0 +1,51 @@
+import { initEventListeners, handleDeleteFile } from './handlers/fileHandlers.js';
+import { getSelectedFiles } from './state/fileState.js';
+import { viewFile as previewFile, closePreview, downloadFile as downloadFilePreview, downloadCurrentPreviewFile } from './components/filePreview/index.js';
+import { initSidebar } from './components/sidebar/sidebar.js';
+import { initRouter } from './router.js';
+import { loadFiles } from './handlers/myfileHandlers.js';
+import { loadStats } from './handlers/statsHandler.js';
+
+initSidebar();
+
+initRouter();
+
+window.addEventListener('pageLoaded', (e) => {
+    if (e.detail.pageName === 'upload') {
+        initEventListeners();
+    } else if (e.detail.pageName === 'myfile') {
+        loadFiles();
+    } else if (e.detail.pageName === 'stats') {
+        loadStats();
+    }
+});
+
+window.deleteFile = function(index) {
+    handleDeleteFile(index);
+};
+
+window.viewFile = function(index) {
+    const selectedFiles = getSelectedFiles();
+    const file = selectedFiles[index];
+    
+    if (!file) {
+        alert('檔案不存在');
+        return;
+    }
+    
+    previewFile(file, index);
+};
+
+window.closePreview = function() {
+    closePreview();
+};
+
+window.downloadFile = function(index) {
+    const selectedFiles = getSelectedFiles();
+    const file = selectedFiles[index];
+    downloadFilePreview(file);
+};
+
+window.downloadCurrentFile = function() {
+    downloadCurrentPreviewFile();
+};
